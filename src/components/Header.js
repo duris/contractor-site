f49d7a8b7737bbef3fafb2ec13aa6b1231d49a23
Ross <ross@duris.io>
2022-12-05 09:38:36 -0500

    updated name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
   return(
     <header className="app-header">      
       <div className="header-wrap">      
-        <span className="logo">Brocks Usrsem</span>
+        <span className="logo">Brock Usrem</span>
         <Nav/>
       </div>
       <span className="header-bg"></span>
